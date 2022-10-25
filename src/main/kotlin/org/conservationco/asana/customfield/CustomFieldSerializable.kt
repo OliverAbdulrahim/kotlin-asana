2e5cf7b66acf3068c14c76fc89fbb732d3bde867
oliverabdulrahim <oliver.abdulrahim@gmail.com>
2022-10-24 20:42:44 -0600

    Fix: property formatting

--- src/main/kotlin/org/conservationco/asana/customfield/CustomFieldSerializable.kt
+++ src/main/kotlin/org/conservationco/asana/customfield/CustomFieldSerializable.kt
@@ -3,6 +3,4 @@ package org.conservationco.asana.customfield
 
 @Target(AnnotationTarget.PROPERTY)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class CustomFieldSerializable(
-    val name: String,
-)
+annotation class CustomFieldSerializable(val name: String)

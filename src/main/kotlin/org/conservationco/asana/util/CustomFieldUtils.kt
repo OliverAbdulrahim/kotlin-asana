6a2af353e35301bb23ab37d5b3613e691d1157d6
oliverabdulrahim <oliver.abdulrahim@gmail.com>
2022-11-02 22:46:13 -0600

    Refactor: move custom field utilities functions and move no-op data to custom field stub

--- src/main/kotlin/org/conservationco/asana/util/CustomFieldUtils.kt
+++ src/main/kotlin/org/conservationco/asana/util/CustomFieldUtils.kt
@@ -1,24 +1,12 @@
 package org.conservationco.asana.util
 
 import com.asana.models.CustomField
+import com.asana.models.CustomField.EnumOption
 import com.asana.models.Resource
 import org.conservationco.asana.serialization.customfield.ResourceSubtype
 import org.conservationco.asana.serialization.customfield.context.CustomFieldContext
 
-fun CustomField.selectMultiEnumOptions(vararg optionsToSelect: String) {
-    val options = enumOptions.filter { optionsToSelect.contains(it.name) }
-    multiEnumValues = options
-}
-
-fun CustomField.multiEnumToGids(): Array<String> = if (multiEnumValues == null) arrayOf("") else multiEnumValues.toGidArray()
-
-fun CustomField.isMultiEnum(): Boolean = this.resourceSubtype == "multi_enum"
-
-fun CustomField.isEnum(): Boolean = this.resourceSubtype == "enum"
-
-fun CustomField.isNumber(): Boolean = this.resourceSubtype == "number"
-
-fun CustomField.isText(): Boolean = this.resourceSubtype == "text" || this.resourceSubtype == "people"
+// public CustomField extension functions
 
 /**
  * Finds the data stored in a given [CustomField] based on its resource subtype. Resource subtypes that we care
@@ -30,15 +18,40 @@ fun CustomField.isText(): Boolean = this.resourceSubtype == "text" || this.resou
  */
 fun CustomField.inferValue(context: CustomFieldContext): Any? = asResourceSubtype(context).convertToData(this)
 
+fun CustomField.isMultiEnum(): Boolean = this.resourceSubtype == "multi_enum"
+
+fun CustomField.isEnum(): Boolean = this.resourceSubtype == "enum"
+
+fun CustomField.isNumber(): Boolean = this.resourceSubtype == "number"
+
+fun CustomField.isText(): Boolean = this.resourceSubtype == "text" || this.resourceSubtype == "people"
+
+// internal CustomField utility functions
+
 internal fun CustomField.asResourceSubtype(context: CustomFieldContext): ResourceSubtype {
     val typeConstructor = ResourceSubtype.types[resourceSubtype]!!
     return if (typeConstructor.parameters.isEmpty()) typeConstructor.call()
     else typeConstructor.call(context)
 }
 
-// Collection functions
+internal fun enumOptionOf(name: String, gid: String): EnumOption = EnumOption().apply {
+    this.name = name
+    this.gid = gid
+}
+
+internal fun customFieldOf(
+    name: String,
+    resourceSubtype: String,
+    enumOptions: Collection<EnumOption> = emptyList()
+): CustomField = CustomField().apply {
+    this.name = name
+    this.resourceSubtype = resourceSubtype
+    this.enumOptions = enumOptions
+}
 
 internal fun Collection<CustomField>.mapGidsToValues(context: CustomFieldContext): Map<String, Any?> =
     associateBy({ it.gid }, { it.inferValue(context) })
 
 internal fun Collection<Resource>.toGidArray(): Array<String> = map { it.gid }.toTypedArray()
+
+internal fun CustomField.multiEnumToGids(): Array<String> = if (multiEnumValues == null) arrayOf("") else multiEnumValues.toGidArray()
--- src/test/kotlin/org/conservationco/asana/serialization/stubs/PersonCustomFieldContextStub.kt
+++ src/test/kotlin/org/conservationco/asana/serialization/stubs/PersonCustomFieldContextStub.kt
@@ -1,18 +1,36 @@
 package org.conservationco.asana.serialization.stubs
 
 import com.asana.models.CustomField
-import com.asana.models.CustomField.EnumOption
 import org.conservationco.asana.serialization.customfield.context.CustomFieldContext
+import org.conservationco.asana.serialization.stubs.PersonCustomFields.personEnumCustomField
+import org.conservationco.asana.serialization.stubs.PersonCustomFields.personMultiEnumField
+import org.conservationco.asana.serialization.stubs.PersonCustomFields.personTextCustomField
+import org.conservationco.asana.util.customFieldOf
+import org.conservationco.asana.util.enumOptionOf
 
 internal class PersonCustomFieldContextStub : CustomFieldContext() {
     override fun loadCustomFields(): Map<String, CustomField> {
-        val textField = getTextCustomField()
-        val enumField = getEnumCustomField()
-        val multiEnumField = getMultiEnumField()
+        val textField = personTextCustomField()
+        val enumField = personEnumCustomField()
+        val multiEnumField = personMultiEnumField()
         return mapOf(
             textField.name to textField,
             enumField.name to enumField,
             multiEnumField.name to multiEnumField
         )
     }
+}
+
+internal object PersonCustomFields {
+    internal const val noOpGid = "1234567890"
+    private val languages =
+        arrayOf("Turkish", "French", "Yoruba", "English", "Vietnamese", "Korean", "Persian", "Inuktitut", "Spanish")
+            .map { enumOptionOf(it, noOpGid) }
+    private val seasons =
+        arrayOf("Winter", "Spring", "Summer", "Fall")
+            .map { enumOptionOf(it, noOpGid) }
+
+    internal fun personTextCustomField() = customFieldOf("Favorite dessert", "text")
+    internal fun personEnumCustomField() = customFieldOf("Favorite season", "enum", seasons)
+    internal fun personMultiEnumField() = customFieldOf("Languages spoken", "multi_enum", languages)
 }

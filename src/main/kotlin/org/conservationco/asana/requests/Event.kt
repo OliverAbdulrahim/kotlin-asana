17777dbeef34977101eec019d027f2cd0bd95d01
oliverabdulrahim <oliver.abdulrahim@gmail.com>
2022-11-18 12:27:37 -0700

    Documentation: update KDoc for Event with property info

--- src/main/kotlin/org/conservationco/asana/requests/Event.kt
+++ src/main/kotlin/org/conservationco/asana/requests/Event.kt
@@ -3,7 +3,14 @@ package org.conservationco.asana.requests
 import com.google.gson.JsonElement
 
 /**
- * Encapsulates Asana events
+ * Encapsulates a single Asana event.
+ *
+ * For more details,
+ * [see the corresponding section of the Asana documentation](https://developers.asana.com/docs/get-events-on-a-resource)
+ *
+ * @property resourceGid The [com.asana.models.Resource] that this event occurred on.
+ * @property changeType The type of change that occurred.
+ * @property changeBody The JSON body containing what changed.
  */
 data class Event(
     val resourceGid: String,

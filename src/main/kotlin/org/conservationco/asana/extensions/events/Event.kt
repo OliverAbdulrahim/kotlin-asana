f68149f7ab24a73b369b9534388c2937e4674a6c
oliverabdulrahim <oliver.abdulrahim@gmail.com>
2022-11-19 17:56:22 -0700

    Breaking change: drop JSON element from Event. This is a tough change to make, but there a few reasons why.
    First, the Asana API returns many duplicates for any given event. Since Event is a data class, we'd like to have equivalent hashcodes for equivalent events. However, there's no simple way to accomplish that AND hold the JSON body as a property of Event objects.
    
    Second, the JSON body is marginally useful, and only for change events.

--- src/main/kotlin/org/conservationco/asana/extensions/events/Event.kt
+++ src/main/kotlin/org/conservationco/asana/extensions/events/Event.kt
@@ -1,7 +1,5 @@
 package org.conservationco.asana.extensions.events
 
-import com.google.gson.JsonElement
-
 /**
  * Encapsulates a single Asana event.
  *
@@ -10,10 +8,8 @@ import com.google.gson.JsonElement
  *
  * @property resourceGid The [com.asana.models.Resource] that this event occurred on.
  * @property type The type of change that occurred.
- * @property eventBody The JSON body containing what changed.
  */
 data class Event(
     val resourceGid: String,
     val type: Action,
-    val eventBody: JsonElement,
 )

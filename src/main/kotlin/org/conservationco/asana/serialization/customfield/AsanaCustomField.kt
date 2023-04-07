8db6a677e3b47c0e737cf9aeafa3aace21cc195a
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2023-04-06 23:38:53 -0600

    Feature: flag to optionally process a field

--- src/main/kotlin/org/conservationco/asana/serialization/customfield/AsanaCustomField.kt
+++ src/main/kotlin/org/conservationco/asana/serialization/customfield/AsanaCustomField.kt
@@ -2,4 +2,7 @@ package org.conservationco.asana.serialization.customfield
 
 @Target(AnnotationTarget.PROPERTY, AnnotationTarget.FIELD)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class AsanaCustomField(val name: String)
+annotation class AsanaCustomField(
+    val name: String,
+    val isOptional: Boolean = false,
+)

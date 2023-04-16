1da279f2c3b0f71a3c92e8d363f165c2d0d3c851
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2023-04-16 12:45:49 -0600

    Feature: accept upper snake case or lower snake case environment variables

--- src/main/kotlin/org/conservationco/asana/ClientDefaults.kt
+++ src/main/kotlin/org/conservationco/asana/ClientDefaults.kt
@@ -5,13 +5,17 @@ import org.conservationco.asana.exception.NoSuppliedAccessTokenException
 import org.conservationco.asana.extensions.AsanaClientExtension
 
 object ClientDefaults {
-    val CONFIG by lazy { AsanaConfig() }
-    val CLIENT by lazy { Client.accessToken(getAccessTokenFromEnv()) }
+    private val CONFIG by lazy { AsanaConfig() }
+    val CLIENT: Client by lazy { Client.accessToken(getAccessTokenFromEnv()) }
     val CLIENT_EXT by lazy { AsanaClientExtension(CONFIG) }
 
     private fun getAccessTokenFromEnv(): String {
-        return System.getenv("asana_access_token") ?: throw NoSuppliedAccessTokenException("""
-            You must pass the "asana_access_token" environment variable when using the default client.
-        """.trimIndent())
+        return System.getenv("ASANA_ACCESS_TOKEN")
+            ?: (System.getenv("asana_access_token")
+            ?: throw NoSuppliedAccessTokenException(
+                "You must pass an environment variable with the key 'ASANA_ACCESS_TOKEN' or 'asana_access_token', " +
+                "or you must provide your own com.asana.Client!"
+        ))
     }
+
 }
